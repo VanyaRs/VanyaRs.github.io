24f4e768d41560fa40266dc07be565a58b3b4ec7
vanyaRs <wrosoha@gmail.com>
2018-07-14 21:48:43 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+ $(".item").magnificPopup({
+ 	type: 'image', gallery : {
+ 		enabled : true
+ 	}, 
+ 	removalDelay: 300,
+ 	mainClass: 'mfp-fade'
+ });
+ 	// body... 	
+})
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
