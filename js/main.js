a4b96805fa751a1d328bc8b2de05e901672c739c
vanyaRs <wrosoha@gmail.com>
2018-07-15 22:34:41 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -7,22 +7,5 @@ $(document).ready(function () {
  	mainClass: 'mfp-fade'
  });
  	// body... 	
- 	$("form.callback").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "/mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			$(th).find('succesfon').addClass('active').hide().fadeIn();
-			$(th).find('succes').css('display','flex').hide().fadeIn();
-			setTimeout(function() {
-			$(th).find(.'succesfon').removeClass('active').fadeout();
-				th.trigger("reset");
-			}, 3000);
-		});
-		return false;
-	});
 })
-
 	//E-mail Ajax Sen
